a50280f4ba989d09bd616ecaaa21725b03374e39
Guest User <Guest@Epicodus-9A.local>
2018-06-13 20:55:37 -0700

    adds

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,11 @@
 
 
 $(document).ready(function() {
-  $("#blanks").submit(function(event) {
+  $("#blanks form").submit(function(event) {
     var item1Input = $("input#item1").val();
     var item2Input = $("input#item2").val();
-
+    var listItems = [item1Input, item1Input]
+    listItems.sort();
     $("#list-item1").append(item1Input);
     $("#list-item2").append(item2Input);
     $(".list").show();
