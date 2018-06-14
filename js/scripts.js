2d2ed11e85f3dcb428416f62b610465ace5a3166
Guest User <Guest@Epicodus-9A.local>
2018-06-13 21:02:27 -0700

    adds scripts to js

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,15 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var item1Input = $("input#item1").val();
     var item2Input = $("input#item2").val();
-    var listItems = [item1Input, item1Input]
+    var listItems = [item1Input, item2Input]
     listItems.sort();
-    $("#list-item1").append(item1Input);
-    $("#list-item2").append(item2Input);
+    console.log(listItems);
+    
+    listItems.forEach(function(listItem){
+      $("#list-item1").append("<li>" + listItem + "</li>");
+    })
+    //$("#list-item1").append(item1Input);
+    //$("#list-item2").append(item2Input);
     $(".list").show();
     event.preventDefault();
   });
